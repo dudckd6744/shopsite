2d35a88725ee9b3787bab5383d3e88f8e83e4406
우영창 <dudckd@uyeongchang-ui-MacBookPro.local>
2021-04-25 11:42:24 +0900

    got

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multerS3 = require("multer-s3");
+const aws = require('aws-sdk');
 const router = express.Router();
 const multer = require('multer');
 const { Product } = require("../models/Product");
@@ -9,15 +11,32 @@ const {User} = require('../models/User');
 //=================================
 //             Product
 //=================================
+const s3 = new aws.S3({
+    accessKeyId: "AKIA4HIE7Q3SRWQM2IIG",   // user 만들면서 지급받은 키값
+    secretAccessKey: "kREjsLKyIcJ/jVJX+FNgxGprbuEUI4ajUfyGDbfu",
+    region: 'ap-northeast-2'
+})
 
-var storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null, '/home/ubuntu/showsite/uploads/')
+// var storage = multer.diskStorage({
+//     destination: function(req,file,cb){
+//         cb(null, '/home/ubuntu/showsite/uploads/')
+//     },
+//     filename: function(req,file,cb){
+//         cb(null, `${Date.now()}_${file.originalname}`)
+//     }
+// })
+const storage = multerS3({
+    s3: s3,
+    bucket: 'elasticbeanstalk-ap-northeast-2-840213432037', // s3 생성시 버킷명
+    acl: 'public-read',   // 업로드 된 데이터를 URL로 읽을 때 설정하는 값입니다. 업로드만 한다면 필요없습니다.
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname}); // 파일 메타정보를 저장합니다.
     },
-    filename: function(req,file,cb){
-        cb(null, `${Date.now()}_${file.originalname}`)
+    key: function (req, file, cb) {
+      cb(null, `${Date.now()}_${file.originalname}`) // key... 저장될 파일명과 같이 해봅니다.
     }
 })
+
 
 var upload = multer({ storage:storage}).single("file")
 
